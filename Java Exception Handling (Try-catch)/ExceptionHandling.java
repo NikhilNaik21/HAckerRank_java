d3698bcd29b20930e4eeffc5d6284dae9161229f
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-06-30 22:43:15 +0530

    Update ExceptionHandling.java

--- Java Exception Handling (Try-catch)/ExceptionHandling.java
+++ Java Exception Handling (Try-catch)/ExceptionHandling.java
@@ -1,1 +1,33 @@
 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+            Scanner in = new Scanner(System.in);
+           
+        try{
+                int x =in.nextInt();
+                int y =in.nextInt();
+           int result = x/y;
+            System.out.println(result);
+        }
+        catch( InputMismatchException e)
+        {
+           System.out.println("java.util.InputMismatchException");
+        }
+        
+        catch( ArithmeticException e1)
+        {
+           System.out.println("java.lang.ArithmeticException: / by zero");
+        }
+        
+        
+       
+    }
+}
