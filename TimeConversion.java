fa69840be8b68f5c6f04e593b6ee932d0b81d91e
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-11-04 11:29:26 +0530

    Create TimeConversion.java

---
+++ TimeConversion.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String timeConversion(String s)  {
+    // Write your code here
+    
+    DateFormat dateFormat = new SimpleDateFormat("hh:mm:ssaa");
+     DateFormat format =  new SimpleDateFormat("HH:mm:ss");
+     
+     Date time = null;
+     String output = "";
+     try{
+        time = dateFormat.parse(s);
+        output = format.format(time);
+     }
+     catch(Exception e)
+     {
+      
+        System.out.println(e);
+     }
+        return output;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
